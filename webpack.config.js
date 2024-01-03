67ec1b0adab9470e9797d761637592279154abac
Pcjmy <1716045191@qq.com>
2024-01-03 23:51:20 +0800

    feat: plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
-    main: './src/index.js',
+    index: './src/index.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
-    ]
-  }
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    })
+  ]
 };
