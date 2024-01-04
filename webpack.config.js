42883465aa7c0a33726e2ecac7ca3fc1da29af68
Pcjmy <1716045191@qq.com>
2024-01-05 00:02:30 +0800

    feat: webpack处理CSS文件

--- webpack.config.js
+++ webpack.config.js
@@ -5,36 +5,22 @@ module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
-    search: './src/search.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
   plugins: [
-    // 单入口
-    // new HtmlWebpackPlugin({
-    //   template: './index.html',
-    // })
-
-    // 多入口
     new HtmlWebpackPlugin({
       template: './index.html',
-      filename: 'index.html',
-      chunks: ['index'],
-      minify: {
-        // 删除注释
-        removeComments: true,
-        // 删除空格
-        collapseWhitespace: true,
-        // 删除html标签属性值的双引号
-        removeAttributeQuotes: true,
-      }
-    }),
-    new HtmlWebpackPlugin({
-      template: './search.html',
-      filename: 'search.html',
-      chunks: ['search'],
     })
   ]
 };
